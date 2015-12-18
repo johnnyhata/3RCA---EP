12a3b60293f96d07d0fcf47c2427a2c8d011617c
johnnyhata <36016@alunos.bandtec.com.br>
2015-12-18 21:02:20 -0200

    Update base.sh

--- base.sh
+++ base.sh
@@ -1,9 +1,11 @@
+#!/bin/bash
 #...............................................................................
 # Programa  : <nome do script>
 # Autor     : <nome de quem escreveu o script>
 # Descrição : <descreva qual o objetivo do script, o que ele faz, qual o
 #             problema que ele resolve, etc.>
 # Observação: <Opcional - escreva se tiver alguma observação a fazer>
-# Logs      : Data                - Descrição
-#             <data da alteração>   <descrição do que foi alterado>
+# Logs      : <alterações que foram feitas nesse código, colocando data e
+#             descrição do que foi feito incluindo a linha>
 #...............................................................................
+
