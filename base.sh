c7b0ebeacc8bb3d2faab8b0a1330e4dc73471260
johnnyhata <36016@alunos.bandtec.com.br>
2015-12-18 20:55:51 -0200

    Arquivo modelo para criação dos scripts
    Modelo de Boas Praticas para escrever os scripts. Os objetivos a serem alcançados são para que todos consigam:
    - Criar um padrão;
    - Entender o código;
    - Fazer a manutenção quando necessário;
    - Encontrar o problema do código rapidamente;
    -

---
+++ base.sh
@@ -0,0 +1,9 @@
+#...............................................................................
+# Programa  : <nome do script>
+# Autor     : <nome de quem escreveu o script>
+# Descrição : <descreva qual o objetivo do script, o que ele faz, qual o
+#             problema que ele resolve, etc.>
+# Observação: <Opcional - escreva se tiver alguma observação a fazer>
+# Logs      : Data                - Descrição
+#             <data da alteração>   <descrição do que foi alterado>
+#...............................................................................
