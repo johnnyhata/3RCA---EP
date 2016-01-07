1f05258ea6db6db6bdc394ffb754d90b2d56fe11
JH2016 <johnnyhata@hotmail.com>
2016-01-07 19:02:44 -0200

    Novo código
    Foi alterado bastante coisa do código.

--- script.sh
+++ script.sh
@@ -1,61 +1,14 @@
 #!/bin/bash
-#
-# Arquivo: script.sh
-# Autor  : Johnny Hata
-# Descri.: Script base arquitetado para fazer integracoes (bibliotecas, UX,
-#          outros script, etc.) de todos os codigos do projeto.
-#          Sera necessario alterar as linhas de testes e correcoes indicadas
-#          no codigo abaixo, de acordo com o entendimento do documento CIS.
-# Logs   : 040115: Primeira versao do codigo.
-#
+PCT=0
+MENSAGEM=$(head -n12 telas.sh)
+( 
+while test $PCT != 100; do
 
-# Declaracao de bibliotecas e variaveis
-. lib.sh                     # Carrega a biblioteca de funcoes
-declare -i nResTestes=0      # Contador de erros dos testes
-declare -i nResCorrecoes=0   # Contador de erros das correcoes
-declare -i nTesAtual=1       # Diz se e a primeira ou segunda vez dos testes
-cScrAtual="$0"               # Nome do script atual para o log.
-cSaida="-tela"               # Define a saida padrao para a tela
-[ "$1" == "-arquivo" ] && cSaida="$1"   # Se especificado a saida sera arquivo
+   # O codigo abaixo é apenas uma demonstração
+   PCT=`expr $PCT + 10`;
+   TITULO=$"Executando script numero $PCT"
+   echo $PCT;
+   echo $TITULO;
+   sleep 1;
 
-for (( nTesAtual=1; nTesAtual<=2; nTesAtual++ ))
-do
-   # Reseta o contador de erros da execucao dos testes
-   nResTestes=0
-
-   # Inicio dos codigos dos testes (ALTERE AQUI)
-   echo "Codigo de Teste 1"   ; nResTestes="$nResTestes"+"$?"
-   echo "Codigo de Teste 2"   ; nResTestes="$nResTestes"+"$?"
-   echo "Codigo de Teste ..." ; nResTestes="$nResTestes"+"$?"
-   # Fim dos codigos dos testes (ALTERE ATE AQUI)
-
-   # Se algum teste falhar E o teste é feito pela primeira vez
-   if [ "$nResTestes" -gt 0 ] && [ "$nTesAtual" -eq 1 ] ; then
-
-      logEvento "$cScrAtual: Foram encontradas falhas de segurança." -erro $cSaida
-
-      # Inicio dos codigos de correcoes (ALTERE AQUI)
-      echo "Codigo de Correcao 1"   ; nResCorrecoes="$nResCorrecoes"+"$?"
-      echo "Codigo de Correcao 2"   ; nResCorrecoes="$nResCorrecoes"+"$?"
-      echo "Codigo de Correcao ..." ; nResCorrecoes="$nResCorrecoes"+"$?"
-      # Fim dos codigos de correcoes (ALTERE ATE AQUI)
-
-      # Se alguma correcao falhar
-      if [ "$nResCorrecoes" -gt 0 ] ; then
-         logEvento "$cScrAtual: Alguma correção falhou. Verifique!" -erro $cSaida
-         break
-      else
-         logEvento "$cScrAtual: As correções foram aplicadas." -concluido $cSaida
-      fi
-
-   # Se algum teste falhar E o teste é feito pela segunda vez
-   elif [ "$nResTestes" -gt 0 ] && [ "$nTesAtual" -eq 2 ] ; then
-      logEvento "$cScrAtual: Falha nos testes depois das correções. Verifique!" -erro $cSaida
-
-   # Se nenhum teste falhou
-   else
-      logEvento "$cScrAtual: Os testes não acusaram falhas de segurança." -concluido $cSaida
-      break
-
-   fi
-done
+done; ) | dialog --title "$TITULO" --gauge "$PCT" 20 70 0 --and-widget
