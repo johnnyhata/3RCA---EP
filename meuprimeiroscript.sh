3627393bdee63b801a5e28643c9d467ab4c188af
JH2016 <johnnyhata@hotmail.com>
2016-01-07 19:40:26 -0200

    Create meuprimeiroscript.sh
    Primeira versão do script.

---
+++ meuprimeiroscript.sh
@@ -0,0 +1,15 @@
+PCT=0
+NOMESCRIPT[0]="ola.sh"
+( 
+while test $PCT != 100; do
+
+   # Exemplo de mudanca 
+
+de mensagem de script
+   if [ $PCT -lt 50 ]; then
+      MENSAGEM=$(cat telas.sh);
+ 
+
+  else
+      MENSAGEM=$(cat telas2.sh);
+   fi
